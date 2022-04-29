b157f4c2c5afb2715fea207879653bd237b4efe1
Elie60 <elmusoft@gmail.com>
2022-04-29 18:27:09 +0000

    Seconde commit

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,19 @@
+from multiprocessing import connection
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+from decouple import config
+from sqlalchemy import create_engine
 
-perc_heads = st.number_input(label = 'Chance of Coins Landing on Heads', min_value = 0.0, max_value = 1.0, value = .5)
-binom_dist = np.random.binomial(1, perc_heads, 1000)
-list_of_means = []
-for i in range(0, 1000):
-    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
-fig, ax = plt.subplots()
 
-ax = plt.hist(list_of_means, range=[0,1])
-st.pyplot(fig)
+st.title("Dashboard")
+#engine_pass=config('')
+#e=create_engine(engine_pass)
+#connection = e.raw_connection() 
+
+data=pd.read_csv('data/data.cvs')
+st.write(data.head())
+
+
+
 
 
---
+++ data/data_schema.sql
@@ -0,0 +1,28 @@
+-- TABLE
+CREATE TABLE Article
+(noArticle INTEGER NOT NULL,
+description VARCHAR(20),
+prixUnitaire DECIMAL(10,2) NOT NULL,
+quantitéEnStock INTEGER DEFAULT 0 NOT NULL
+CHECK (quantitéEnStock >= 0),
+PRIMARY KEY (noArticle));
+CREATE TABLE Client (noClient INTEGER NOT NULL, nomClient VARCHAR(20) NOT NULL,noTéléphone VARCHAR(15) NOT NULL,PRIMARY KEY (noClient));
+CREATE TABLE Commande (noCommande INTEGER NOT NULL, dateCommande DATE NOT NULL, noClient INTEGER NOT NULL, PRIMARY KEY (noCommande),FOREIGN  KEY (noClient) REFERENCES Client );
+CREATE TABLE DétailLivraison
+(noLivraison INTEGER NOT NULL,
+noCommande INTEGER NOT NULL,
+noArticle INTEGER NOT NULL,
+quantitéLivrée INTEGER NOT NULL
+CHECK (quantitéLivrée > 0),
+PRIMARY KEY (noLivraison, noCommande, noArticle),
+FOREIGN KEY (noLivraison) REFERENCES Livraison,
+FOREIGN KEY (noCommande, noArticle) REFERENCES LigneCommande);
+CREATE TABLE LigneCommande (noCommande INTEGER NOT NULL,noArticle INTEGER NOT NULL, quantité INTEGER NOT NULL CHECK (quantité > 0), PRIMARY KEY (noCommande, noArticle), FOREIGN KEY (noCommande) REFERENCES Commande, FOREIGN KEY (noArticle) REFERENCES Article );
+CREATE TABLE Livraison (noLivraison INTEGER NOT NULL, dateLivraison DATE NOT NULL, PRIMARY KEY (noLivraison));
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
